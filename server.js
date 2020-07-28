90840a36416e9d88d3e4e5d349c499e1c28ef58b
paul-hanneforth <paul.hanneforth.o@gmail.com>
2020-07-28 18:36:37 +0200

    initial commit

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const arcticNode = require("arctic-node");
+
+const app = express();
+const port = 81;
+
+const appID = "id2xy901kf"
+const appToken = "w7udj9k0mu"
+const callbackURL = "http://arcticxyz.ddns.net:81/"
+
+app.use("/", arcticNode(appToken))
+
+app.get("/", (req, res) => {
+
+    if(!req.authenticated) res.authenticate(appID, callbackURL)
+
+    res.send("Hello! " + req.user.name + " | " + req.user.email + " | " + req.user.id)
+
+})
+
+app.listen(port, () => { console.log("Server started!") })
